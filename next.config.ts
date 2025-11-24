fa4c459d20bdf7f0f27aa3b7ab183400d626da9c
Devran Sever <137220822+DevranTheDeveloper@users.noreply.github.com>
2025-11-24 16:46:11 +0300

    feat: add new remote image patterns for various services and categorize all patterns.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Google Services
       {
         protocol: 'https',
         hostname: 'drive.google.com',
@@ -13,16 +14,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'res.cloudinary.com',
+        hostname: 'storage.googleapis.com',
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'unsplash.com',
-      },
+
+      // GitHub
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
@@ -33,16 +28,98 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'user-images.githubusercontent.com',
+      },
+
+      // Unsplash
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'unsplash.com',
+      },
+
+      // Instagram
+      {
+        protocol: 'https',
+        hostname: 'instagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'scontent-dus1-1.cdninstagram.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cdninstagram.com', // Wildcard for all CDN regions
+      },
+
+      // Facebook
+      {
+        protocol: 'https',
+        hostname: 'scontent.xx.fbcdn.net',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net', // Wildcard for all Facebook CDNs
+      },
+
+      // LinkedIn
+      {
+        protocol: 'https',
+        hostname: 'media.licdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media-exp1.licdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.licdn.com',
+      },
+
+      // Twitter
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'abs.twimg.com',
+      },
+
+      // Cloudinary
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+
+      // Imgur
+      {
+        protocol: 'https',
         hostname: 'i.imgur.com',
       },
       {
         protocol: 'https',
         hostname: 'imgur.com',
       },
+
+      // Image Proxy
       {
         protocol: 'https',
-        hostname: 'wsrv.nl', // Image proxy for other URLs
+        hostname: 'wsrv.nl',
       },
+
+      // Other CDNs
       {
         protocol: 'https',
         hostname: 'imagekit.io',
@@ -53,11 +130,51 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'pbs.twimg.com',
+        hostname: 'images.pexels.com',
       },
       {
         protocol: 'https',
-        hostname: 'abs.twimg.com',
+        hostname: 'images.ctfassets.net', // Contentful
+      },
+
+      // AWS & Cloud Storage
+      {
+        protocol: 'https',
+        hostname: '**.s3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.cloudfront.net', // AWS CloudFront
+      },
+
+      // Others
+      {
+        protocol: 'https',
+        hostname: 'gravatar.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'secure.gravatar.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com', // YouTube thumbnails
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com', // YouTube profile pics
       },
     ],
   },
